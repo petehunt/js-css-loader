17c6c4bc12ca4ad701bf7e55dc540430e762d9bf
petehunt <floydophone@gmail.com>
2014-02-03 02:41:48 -0800

    it works

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 var CSSPropertyOperations = require('react/lib/CSSPropertyOperations');
 
+var path = require('path');
+
 module.exports = function(content) {
 	this.cacheable && this.cacheable();
-  console.log(this.resource);
 
+  var stylesheet = require(path.resolve(this.resource));
+  var css = '';
+  for (var k in stylesheet) {
+    if (!stylesheet.hasOwnProperty(k)) {
+      continue;
+    }
+    css += '.' + k + ' {\n' + CSSPropertyOperations.createMarkupForStyles(stylesheet[k]) + '\n}\n';
+  }
+
+  return css;
 };
